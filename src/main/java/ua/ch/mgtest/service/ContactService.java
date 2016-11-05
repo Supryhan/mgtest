66a4ba97ba80249adae354ffb0e312f1c85a88c3
Vitalii Supryhan <suprigan@gmail.com>
2016-11-05 23:04:58 +0200

    make services transactional

--- src/main/java/ua/ch/mgtest/service/ContactService.java
+++ src/main/java/ua/ch/mgtest/service/ContactService.java
@@ -2,6 +2,7 @@ package ua.ch.mgtest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.ch.mgtest.dao.ContactDao;
 import ua.ch.mgtest.dao.SequenceDao;
 import ua.ch.mgtest.model.Contact;
@@ -10,28 +11,35 @@ import java.util.List;
 
 @Service
 public class ContactService {
+
 //	@Autowired
 //	private SequenceDao sequenceDao;
+
 	@Autowired
 	private ContactDao contactDao;
 
+	@Transactional
 	public void add(Contact contact) {
 //		contact.setId(sequenceDao.getNextSequenceId(Contact.COLLECTION_NAME));
 		contactDao.save(contact);
 	}
 
+	@Transactional
 	public void update(Contact contact) {
 		contactDao.save(contact);
 	}
 
+	@Transactional
 	public Contact get(String id) {
 		return contactDao.get(id);
 	}
 
+	@Transactional
 	public List<Contact> getAll() {
 		return contactDao.getAll();
 	}
 
+	@Transactional
 	public void remove(Long id) {
 		contactDao.remove(id);
 	}
--- src/main/java/ua/ch/mgtest/service/NetworkScanService.java
+++ src/main/java/ua/ch/mgtest/service/NetworkScanService.java
@@ -2,6 +2,7 @@ package ua.ch.mgtest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.ch.mgtest.dao.UriDao;
 import ua.ch.mgtest.model.Uri;
 
@@ -11,19 +12,21 @@ import java.util.List;
 @Service
 public class NetworkScanService {
 
-    @Autowired
-    private UriDao uriDao;
+	@Autowired
+	private UriDao uriDao;
 
-    public void saveDestinations(List<String> destinations) {
-        List<Uri> uriList = new LinkedList<>();
-        destinations.forEach(destination -> uriList.add(new Uri(destination)));
-        uriDao.saveAll(uriList);
-    }
+	@Transactional
+	public void saveDestinations(List<String> destinations) {
+		List<Uri> uriList = new LinkedList<>();
+		destinations.forEach(destination -> uriList.add(new Uri(destination)));
+		uriDao.saveAll(uriList);
+	}
 
-    public List<String> getDestinations() {
-        List<String> destinationList = new LinkedList<>();
-        uriDao.getAll().forEach(destination -> destinationList.add(destination.getUri()));
-        return destinationList;
-    }
+	@Transactional
+	public List<String> getDestinations() {
+		List<String> destinationList = new LinkedList<>();
+		uriDao.getAll().forEach(destination -> destinationList.add(destination.getUri()));
+		return destinationList;
+	}
 
 }
